926791d3e4829ba108aa27a7bd399e04bd1464ef
destrosw <grigorievivan.1@mail.ru>
2019-04-07 17:29:06 +0300

    fix import script and delete export script

--- app/static/js/export.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('#navbar').css('display', 'none');
-	$.get('/export', function(data) {
-		$('#navbar').css('display', 'block');
-		alert('Загрузка завершена.');
-	});
-});
--- app/static/js/import.js
+++ app/static/js/import.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-	$('#navbar').css('display', 'none');
-	$.get('/import', function(data) {
-		$('#navbar').css('display', 'block');
-		alert('Загрузка завершена.');
-	});
+	$('#modal1').css('display', 'block');
+
+	$('#btn-cancel').on('click', function() {
+        $('#navbar').css('display', 'block');
+		$('#fountainTextG').css('display', 'none');
+		$('#status').text('Импорт отменен')
+    });
+
+    $('#btn-accept').on('click', function() {
+        $('#modal1').css('display', 'none');
+        $('#navbar').css('display', 'none');
+
+        $.get('/import', function(data) {
+			$('#navbar').css('display', 'block');
+			$('#fountainTextG').css('display', 'none');
+			$('#status').text('Загрузка завершена!')
+		});
+    });
 });
