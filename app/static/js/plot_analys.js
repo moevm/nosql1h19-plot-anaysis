f4f75b83775bb4399c827136202bc7cda4357deb
destrosw <grigorievivan.1@mail.ru>
2019-04-14 21:10:33 +0300

    show load animation before actions

--- app/static/js/plot_analys.js
+++ app/static/js/plot_analys.js
@@ -7,9 +7,13 @@ $(document).ready(function(){
     $("#genres_but_plot").click(get_table_plot_genre);
 
     function get_table_plot_dir() {
+        $("#words_for_dir").css('display', 'none');
+        $("#cube-loader.words_for_dir").css('display', 'block');
         $.get('/get_table_plot_dir', {
             dir: $('#plot_directors').val()
         }, function (data) {
+            $("#words_for_dir").css('display', 'block');
+            $("#cube-loader.words_for_dir").css('display', 'none');
             $("#tbody_dir_plot").empty();
             for (let i = 0; i < data.length; i++) {
                 let tbody = document.getElementById('tbody_dir_plot');
@@ -28,9 +32,13 @@ $(document).ready(function(){
     }
 
     function get_table_plot_genre() {
+        $("#words_for_genre").css('display', 'none');
+        $("#cube-loader.words_for_genre").css('display', 'block');
         $.get('/get_table_plot_genre', {
             genre: $('#plot_genres').val()
         }, function (data) {
+            $("#words_for_genre").css('display', 'block');
+            $("#cube-loader.words_for_genre").css('display', 'none');
             $("#tbody_genre_plot").empty();
             for (let i = 0; i < data.length; i++) {
                 let tbody = document.getElementById('tbody_genre_plot');
@@ -49,7 +57,11 @@ $(document).ready(function(){
     }
 
     function get_table_universal_words() {
+        $("#universal_words").css('display', 'none');
+        $("#cube-loader.universal_words").css('display', 'block');
         $.get('/get_table_universal_words', function (data) {
+            $("#universal_words").css('display', 'block');
+            $("#cube-loader.universal_words").css('display', 'none');
             $("#tbody_universal_words").empty();
             for (let i = 0; i < data.length; i++) {
                 let tbody = document.getElementById('tbody_universal_words');
