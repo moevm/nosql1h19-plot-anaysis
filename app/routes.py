a0b31d6d60bc0b086c8f670c19b4d00e635b7ca8
Citronnn <mrcitron@mail.ru>
2019-04-14 13:27:28 +0300

    added requests for plot analys

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from string import punctuation
 import os
 from json import dumps
 from flask import Flask, g, Response, request, render_template, send_from_directory
@@ -268,4 +269,66 @@ def get_orig_perc_time():
 
 @app.route("/plot_analys")
 def get_plot_analys_page():
-    return render_template("plot_analys.html")
+    dirs=[]
+    db = get_db()
+    tmp = db.run("match (p:Person)-[:DIRECTED]-(n:Movie) where p.name <> 'Unknown' return  p.name as name,count(n) as count order by count desc limit 50")
+    for dir in tmp:
+        dirs.append(dir['name'])
+    genres=[]
+    tmp = db.run("match(n:Movie) where n.genre <> 'unknown' and not n.genre contains ',' and not trim(n.genre) contains '/' return trim(n.genre) as genre, count(trim(n.genre)) as counts order by counts desc limit 20")
+    for genre in tmp:
+        genres.append(genre['genre'])
+    return render_template("plot_analys.html", dirs=dirs,genres=genres)
+
+def get_stop_words():
+    return ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re","s", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
+
+@app.route("/get_table_plot_dir")
+def get_table_plot_dir():
+    dir = request.args["dir"]
+    stop_words = get_stop_words()
+    db = get_db()
+    data, words_with_count=[],[]
+    texts = db.run("match (n:Movie)-[:DIRECTED]-(p:Person) where p.name = {dir} return n.plot as plot",{'dir':dir})
+    for text in texts:
+        tokens = text['plot'].replace('.','').replace(',','').replace('\"','').replace('\'',' ').replace(':','').replace(';','').lower().split()
+        tokens = [token for token in tokens if token not in stop_words and token != ' ' and token.strip() not in punctuation]
+        for token in tokens:
+            data.append(token)
+    words = db.run('with {data} as words unwind (words) as word return word as word,count(word) as count order by count desc limit 25',{'data':data})
+    for word in words:
+        words_with_count.append([word["word"],word["count"]])
+    return Response(dumps(words_with_count), mimetype="application/json")
+
+@app.route("/get_table_plot_genre")
+def get_table_plot_genre():
+    genre = request.args["genre"]
+    stop_words = get_stop_words()
+    db = get_db()
+    data, words_with_count=[],[]
+    texts = db.run("match (n:Movie) where n.genre contains {genre} return n.plot as plot",{'genre':genre})
+    for text in texts:
+        tokens = text['plot'].replace('.','').replace(',','').replace('\"','').replace('\'',' ').replace(':','').replace(';','').lower().split()
+        tokens = [token for token in tokens if token not in stop_words and token != ' ' and token.strip() not in punctuation]
+        for token in tokens:
+            data.append(token)
+    words = db.run('with {data} as words unwind (words) as word return word as word,count(word) as count order by count desc limit 25',{'data':data})
+    for word in words:
+        words_with_count.append([word["word"],word["count"]])
+    return Response(dumps(words_with_count), mimetype="application/json")
+
+@app.route("/get_table_universal_words")
+def get_table_universal_words():
+    stop_words = get_stop_words()
+    db = get_db()
+    data, words_with_count=[],[]
+    texts = db.run("match (n:Movie) return n.plot as plot")
+    for text in texts:
+        tokens = text['plot'].replace('.','').replace(',','').replace('\"','').replace('\'',' ').replace(':','').replace(';','').lower().split()
+        tokens = [token for token in tokens if token not in stop_words and token != ' ' and token.strip() not in punctuation]
+        for token in tokens:
+            data.append(token)
+    words = db.run('with {data} as words unwind (words) as word return word as word,count(word) as count order by count desc limit 25',{'data':data})
+    for word in words:
+        words_with_count.append([word["word"],word["count"]])
+    return Response(dumps(words_with_count), mimetype="application/json")
