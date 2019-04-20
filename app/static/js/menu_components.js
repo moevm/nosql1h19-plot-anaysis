e8d8b4c973f585bd5e59a50194d354b9bed168c2
Konstantin Zubov <knock1703@gmail.com>
2019-04-21 00:19:13 +0300

    Add files via upload

--- app/static/js/menu_components.js
+++ app/static/js/menu_components.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
     let menu_show_flag = false;
     function menu_show() {
         if (menu_show_flag == false) {
-            $('.butt_menu_components').animate({'margin-right': '300px'});
+            $('.butt_menu_components').animate({'margin-right': '350px'});
+			$('.show').text('Close');
             $('.solo_components_menu').animate({'margin-right': '0px'});
         }
         else {
             $('.butt_menu_components').animate({'margin-right': '0px'});
-            $('.solo_components_menu').animate({'margin-right': '-300px'});
+			$('.show').text('Show');
+            $('.solo_components_menu').animate({'margin-right': '-350px'});
         }
         menu_show_flag = !menu_show_flag;
     }
