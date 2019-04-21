a2c24e7f31fa01a525d0903c40b2a29953e3bf8b
destrosw <grigorievivan.1@mail.ru>
2019-04-21 17:37:14 +0300

    complete import client things

--- app/static/js/import.js
+++ app/static/js/import.js
@@ -2,10 +2,6 @@ $(document).ready(function() {
 	$('#fountainTextG').css('display', 'none');
 	var file;
 
-	$('#file').on('change', function() {
-		$('#status').text('');
-	});
-
 	$('#start-import-btn').on('click', function() {
 		file = $('#file').get(0).files[0];
 		if (file) {
@@ -24,18 +20,40 @@ $(document).ready(function() {
 
     $('#btn-accept').on('click', function() {
         $('#modal1').css('display', 'none');
-        $('#navbar').css('display', 'none');
-        $('#btn-start-import-wrap').css('display', 'none');
-        $('#fountainTextG').css('display', 'block');
 
         $("#file").prop("disabled", true);
 
         $('#status').text('Import of data about 34 Kb can take from 1 to 2 hours. Please wait.')
 
-        $.get('/import', function(data) {
-			$('#navbar').css('display', 'block');
-			$('#fountainTextG').css('display', 'none');
-			$('#status').text('Import completed')
-		});
+        var fd = new FormData;
+        fd.append('csv_import_file', file, 'import.csv');
+
+        $.ajax({
+	        url: '/import',
+	        data: fd,
+	        processData: false,
+	        contentType: false,
+	        type: 'POST',
+	        success: function (data) {
+	        	$('#fountainTextG').css('display', 'block');
+	        	$('#navbar').css('display', 'none');
+        		$('#btn-start-import-wrap').css('display', 'none');
+	            $.get('/import', function(data) {
+					$('#navbar').css('display', 'block');
+					$('#fountainTextG').css('display', 'none');
+					$('#status').text('Import completed');
+				}).fail(function() {
+				    $('#status').text('Import failed. Try again.');
+				    $('#navbar').css('display', 'block');
+				    $('#fountainTextG').css('display', 'none');
+				    $('#btn-start-import-wrap').css('display', 'block');
+				    $("#file").prop("disabled", false);
+				});
+	        },
+	        error: function(error) {
+	        	console.log(error);
+	        	$('#status').text('Cant load file on server');
+	        }
+    	});
     });
 });
