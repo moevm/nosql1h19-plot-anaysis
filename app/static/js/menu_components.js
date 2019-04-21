dff07c65e1cdce8571f7d83c4a6910acfa376086
Konstantin Zubov <knock1703@gmail.com>
2019-04-21 17:42:14 +0300

    js

--- app/static/js/menu_components.js
+++ app/static/js/menu_components.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
     function menu_show() {
         if (menu_show_flag == false) {
             $('.butt_menu_components').animate({'margin-right': '260px'});
-			$('.show').text('Close panel');
+			$('.show').text('Close menu panel for search on graph');
             $('.solo_components_menu').animate({'margin-right': '0px'});
         }
         else {
             $('.butt_menu_components').animate({'margin-right': '0px'});
-			$('.show').text('Show panel');
+			$('.show').text('Show menu panel for search on graph');
             $('.solo_components_menu').animate({'margin-right': '-260px'});
         }
         menu_show_flag = !menu_show_flag;
