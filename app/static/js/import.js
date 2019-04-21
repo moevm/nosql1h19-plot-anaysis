a5f242a19cbff63db8d05f7765abc28eebf7a8ff
destrosw <grigorievivan.1@mail.ru>
2019-04-21 13:20:38 +0300

    import things on client

--- app/static/js/import.js
+++ app/static/js/import.js
@@ -1,21 +1,41 @@
 $(document).ready(function() {
-	$('#modal1').css('display', 'block');
+	$('#fountainTextG').css('display', 'none');
+	var file;
+
+	$('#file').on('change', function() {
+		$('#status').text('');
+	});
+
+	$('#start-import-btn').on('click', function() {
+		file = $('#file').get(0).files[0];
+		if (file) {
+			$('#modal1').css('display', 'block');
+		} else {
+			$('#status').text('file not set');
+		}
+	});
 
 	$('#btn-cancel').on('click', function() {
 		$('#modal1').css('display', 'none');
         $('#navbar').css('display', 'block');
 		$('#fountainTextG').css('display', 'none');
-		$('#status').text('Импорт отменен')
+		$('#status').text('Import canceled')
     });
 
     $('#btn-accept').on('click', function() {
         $('#modal1').css('display', 'none');
         $('#navbar').css('display', 'none');
+        $('#btn-start-import-wrap').css('display', 'none');
+        $('#fountainTextG').css('display', 'block');
+
+        $("#file").prop("disabled", true);
+
+        $('#status').text('Import of data about 34 Kb can take from 1 to 2 hours. Please wait.')
 
         $.get('/import', function(data) {
 			$('#navbar').css('display', 'block');
 			$('#fountainTextG').css('display', 'none');
-			$('#status').text('Загрузка завершена!')
+			$('#status').text('Import completed')
 		});
     });
 });
