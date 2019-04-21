7d2a09d1071ececf1c542a7c0bc7bd900a76de1e
Citronnn <mrcitron@mail.ru>
2019-04-21 16:11:02 +0300

    some changes in style of menu

--- app/static/js/menu_components.js
+++ app/static/js/menu_components.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
     let menu_show_flag = false;
     function menu_show() {
         if (menu_show_flag == false) {
-            $('.butt_menu_components').animate({'margin-right': '200px'});
+            $('.butt_menu_components').animate({'margin-right': '250px'});
 			$('.show').text('Close panel');
             $('.solo_components_menu').animate({'margin-right': '0px'});
         }
         else {
             $('.butt_menu_components').animate({'margin-right': '0px'});
 			$('.show').text('Show panel');
-            $('.solo_components_menu').animate({'margin-right': '-200px'});
+            $('.solo_components_menu').animate({'margin-right': '-250px'});
         }
         menu_show_flag = !menu_show_flag;
     }
